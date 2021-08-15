b03a57312341fe7f2a374650a3ba7d4b5ea57419
wooho <woohobi@gmail.com>
2021-08-16 03:38:12 +0900

    ADD post 테스트 코드

---
+++ coronamap/src/test/java/com/coronamap/coronamap/CoronamapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronamap.coronamap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronamapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
