ad880513d8ed35a9d6b78faea2896de0ed3f8338
wooho <woohobi@gmail.com>
2021-08-16 03:42:15 +0900

    initial commit

---
+++ coronamap/src/main/java/com/coronamap/coronamap/CoronamapApplication.java
@@ -0,0 +1,13 @@
+package com.coronamap.coronamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronamapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronamapApplication.class, args);
+	}
+
+}
