33c666a2ae4df6355bbe075e54bcd21f6af6a25b
wooho <woohobi@gmail.com>
2021-08-18 22:39:03 +0900

    ADD 환자 리스트

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,11 +1,12 @@
 /** @jsxImportSource @emotion/react */
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Preview from "./preview";
 import {css} from "@emotion/react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
 import {placeState} from "../state/state";
 import {useRecoilState} from "recoil";
+import PatientList from "./patient_list";
 
 const sidebarStyle = css`
     flex:1.7;
@@ -16,30 +17,34 @@ const sidebarStyle = css`
 `
 
 const buttonStyle = css`
+    margin-top: 20px;
     width: 200px;
+    height: 20px;
     background-color:white;
     box-shadow: 0 1px 3px 0;
 `
 
-const Sidebar = ({keyword,rest}) =>{
+const Sidebar = ({keyword, rest}) =>{
     const [place, setPlace] = useRecoilState(placeState);
-    const [number, setNumber] = useState({
-        count : 0
-    })
     const [date, setDate] = useState(new Date());
-    const placeDate = { ...place, ...date}
+
+    const [patients, setPatients] = useState({});
+
+    useEffect(()=>{
+        const sync = rest.getPatients()
+            .then(result  => setPatients(result))
+        console.log(place);
+    }, [rest,place])
     const onSubmit =() =>{
+        const placeDate = {
+            ...place,
+            "date": getDate()
+            }
         rest.savePatient(placeDate).then(r => console.log(r));
+        const updated = {...patients, placeDate}
+        setPatients(updated);
     }
-    const onTest =()=>{
-        setNumber((state) => ({
-            count: state.count+1
-        }))
-        setNumber((state) => ({
-            count: state.count+1
-        }))
-        console.log(number.count)
-    }
+
     const getDate = () => {
         const year = date.getFullYear()
         const month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth()
@@ -48,7 +53,7 @@ const Sidebar = ({keyword,rest}) =>{
         return formattedDate;
     }
     return(
-        <div className ="PatientList" css = {sidebarStyle}>
+        <div className ="Sidebar" css = {sidebarStyle}>
             <DatePicker
                 inline
                 dateFormat = "yyyy-MM-dd"
@@ -56,7 +61,8 @@ const Sidebar = ({keyword,rest}) =>{
                 onChange={(date) => setDate(date)}
             />
             <Preview keyword={keyword} />
-            <button onClick={onTest} css={buttonStyle}>{number.count}</button>
+            <input type="button" onClick={onSubmit} value="동선 추가하기" css={buttonStyle}></input>
+            <PatientList patients={patients} />
         </div>
     )
 }
